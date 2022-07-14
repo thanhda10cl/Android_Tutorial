bf8257d908a0f292734cdb394081299b544a8289
NguyenDa <thanhda10cl@gmail.com>
2022-07-14 12:30:24 +0700

    UI basic

--- app/src/main/java/android/com/MainActivity.java
+++ app/src/main/java/android/com/MainActivity.java
@@ -4,24 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG =  MainActivity.class.getSimpleName();
-    Button button;
+    //Button button;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//       setContentView(R.layout.activity_main);
+//    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
-        if (savedInstanceState == null){
-            Log.e(TAG,"Bug");
-        }
-        else {
-            Log.e(TAG,"WELCOME!!");
-        }
-
         setContentView(R.layout.activity_main);
-        //button = new Button(this);
-        button.setText("TEST BUTTON");
+
+        FrameLayout frameLayout =(FrameLayout) findViewById(R.id.frame_layout);
+        TextView product = new TextView(this);
+        product.setText("NguyenThanhDa");
+        product.setTextSize(20);
+        frameLayout.addView(product);
     }
 }
